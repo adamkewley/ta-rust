15976e47e714003831b23d49a83baaf6f9a99d90
Adam Kewley <adam@petagene.com>
2020-02-29 19:30:24 +0000

    Updated example games slightly

--- example_games/shell-script/script.sh
+++ example_games/shell-script/script.sh
@@ -1,6 +1,27 @@
 #!/usr/bin/env bash
 
-echo "Type something in"
+echo "Hi, I'm a basic bash script"
+
+sleep 0.3
+
+echo "Let me just go ahead an expose my internals ;)"
+set -xe
+
+echo "Ah, much better"
+
+echo "Type the correct guess"
 read var
 
-echo "You typed ${var}"
+while [[ "${var}" != 'the correct guess' ]]; do
+      echo "Your answer '${var}' was the wrong guess, try again!"
+      read var
+done
+
+sleep 0.3
+
+echo "DING DING DING! That was the correct guess!"
+
+sleep 0.3
+
+
+sleep 1
